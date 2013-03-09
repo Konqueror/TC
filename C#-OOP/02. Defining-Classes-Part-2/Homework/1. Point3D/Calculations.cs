5e3c3b9735c50f65dcd9a9657100182ef5d8fd02
Ivaylo Teodorov Buchvarov <bachvarof@gmail.com>
2013-03-10 01:09:43 +0200

    Defininf Classes Part 2 homework

---
+++ C#-OOP/02. Defining-Classes-Part-2/Homework/1. Point3D/Calculations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lib
+{
+    class Calculations
+    {
+        // 3. Write a static class with a static method to calculate the distance between two points in the 3D space.
+
+        // I use that algo http://www.calculatorsoup.com/calculators/geometry-solids/distance-two-points.php
+
+        public static double Distance(Point3D PointA, Point3D PointB)
+        {
+            return Math.Sqrt((PointA.x - PointB.x) * (PointA.x - PointB.x) +
+                             (PointA.y - PointB.y) * (PointA.y - PointB.y) +
+                             (PointA.z - PointB.z) * (PointA.z - PointB.z));
+        }
+    }
+}
